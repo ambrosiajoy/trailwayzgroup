3e96f0d6f3bc1f0804260abbcf875a99b315ae6d
ambrosiajoy <ambrosianjoy@survivalwicks.com>
2019-08-21 12:38:26 -0600

    updates

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,14 @@
 import React, {useState} from 'react';
 
+var http = require('http');
+
+function onRequest(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Thank You!');
+    response.end();
+}
+
+http.createServer(onRequest).listen(8000);
 function App(props) {
     
   
